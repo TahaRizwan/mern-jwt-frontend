7a2fa0b7dd2ed317c37b18ea683a431029721a7d
TahaRizwan <tahabhaiyag@gmail.com>
2022-11-01 17:36:46 +0500

    Progress Bar added

--- src/Home.js
+++ src/Home.js
@@ -3,15 +3,12 @@ import WorkoutDetails from './components/WorkoutDetails'
 import WorkoutForm from './components/WorkoutForm'
 import { useDispatch, useSelector } from 'react-redux'
 import { getWorkout } from './workout/workoutAction'
-import CircularProgress from '@mui/material/CircularProgress'
-import { Box } from '@mui/material'
 
 
 const Home = () => {
   const [workouts, setWorkouts] = useState([])
   const dispatch = useDispatch()
   const data = useSelector((state) => state.workout.workout)
-  const loading = useSelector((state) => state.workout.loading)
   useEffect(() => {
     const fetchAllWorkout = async () => {
       dispatch(getWorkout)
@@ -21,20 +18,15 @@ const Home = () => {
 
   useEffect(() => setWorkouts(data), [data])
   return (
-    <div className='home'>
-      <div className='workouts'>
-        {loading && (
-          <Box sx={{ display: 'flex' }}>
-            <CircularProgress />
-          </Box>
-        )}
-        {workouts &&
-          workouts.map((workout) => (
-            <WorkoutDetails key={workout._id} workout={workout} />
-          ))}
+      <div className='home'>
+        <div className='workouts'>
+          {workouts &&
+            workouts.map((workout) => (
+              <WorkoutDetails key={workout._id} workout={workout} />
+            ))}
+        </div>
+        <WorkoutForm />
       </div>
-      <WorkoutForm />
-    </div>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
+import { Box, LinearProgress } from '@mui/material'
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+  const loading = useSelector((state) => state.workout.loading)
   return (
     <header>
+      {loading &&
+        <Box sx={{ width: '100%' }}>
+          <LinearProgress />
+        </Box>
+      }
       <div className='container'>
         <Link to='/'>
           <h1>Workout Buddy</h1>
